da366f6f93fbeb7c4a9a406be54dd42618816f10
Pengfei Ma <mapengfei@mail.nwpu.edu.cn>
2025-07-19 18:27:46 +0800

    [feat] 1. quadrilateral 单元 2. disk driven

---
+++ afsic/src/afsic/euler/dev2/a-4.py
@@ -0,0 +1,76 @@
+import numpy as np
+from mpi4py import MPI
+from petsc4py import PETSc
+from basix.ufl import element
+import dolfinx
+from dolfinx.fem import (Function, functionspace,
+                         dirichletbc, locate_dofs_topological)
+
+from afsic import IPCSSolver
+
+
+with dolfinx.io.XDMFFile(MPI.COMM_WORLD, 'DFG-2D-3-benchmark.xdmf', "r", encoding=dolfinx.io.XDMFFile.Encoding.HDF5) as file:
+    mesh = file.read_mesh()
+    mesh.topology.create_connectivity(mesh.topology.dim-1, mesh.topology.dim)
+    ft = file.read_meshtags(mesh, "Facet markers")
+
+
+gdim = 2
+inlet_marker, outlet_marker, wall_marker, obstacle_marker = 2, 3, 4, 5
+
+
+t = 0
+T = 0.5                       # Final time
+dt = 1 / 1600                 # Time step size
+num_steps = int(T / dt)
+rho = 1.0
+mu = 0.001
+
+v_cg2 = element("Lagrange", mesh.topology.cell_name(), 2, shape=(mesh.geometry.dim, ))
+s_cg1 = element("Lagrange", mesh.topology.cell_name(), 1)
+V = functionspace(mesh, v_cg2)
+Q = functionspace(mesh, s_cg1)
+fdim = mesh.topology.dim - 1
+
+# Define boundary conditions
+class InletVelocity():
+    def __init__(self, t):
+        self.t = t
+
+    def __call__(self, x):
+        values = np.zeros((gdim, x.shape[1]), dtype=PETSc.ScalarType)
+        values[0] = 4 * 1.5 * \
+            np.sin(self.t * np.pi / 8) * x[1] * (0.41 - x[1]) / (0.41**2)
+        return values
+
+
+# Inlet
+u_inlet = Function(V)
+inlet_velocity = InletVelocity(t)
+u_inlet.interpolate(inlet_velocity)
+bcu_inflow = dirichletbc(u_inlet, locate_dofs_topological(V, fdim, ft.find(inlet_marker)))
+# Walls
+u_nonslip = np.array((0,) * mesh.geometry.dim, dtype=PETSc.ScalarType)
+bcu_walls = dirichletbc(u_nonslip, locate_dofs_topological(V, fdim, ft.find(wall_marker)), V)
+# Obstacle
+bcu_obstacle = dirichletbc(u_nonslip, locate_dofs_topological(V, fdim, ft.find(obstacle_marker)), V)
+bcu = [bcu_inflow, bcu_obstacle, bcu_walls]
+# Outlet
+bcp_outlet = dirichletbc(PETSc.ScalarType(0), locate_dofs_topological(Q, fdim, ft.find(outlet_marker)), Q)
+bcp = [bcp_outlet]
+
+# Define Solver
+ns_solver = IPCSSolver(V, Q, bcu, bcp, dt, rho, mu)
+for i in range(num_steps):
+    # Update current time step
+    t += dt
+    # Update inlet velocity
+    inlet_velocity.t = t
+    u_inlet.interpolate(inlet_velocity)
+    ns_solver.solve_one_step()
+
+u_norm = dolfinx.la.norm(ns_solver.u_.x, dolfinx.la.Norm.l2)
+p_norm = dolfinx.la.norm(ns_solver.p_.x, dolfinx.la.Norm.l2)
+if mesh.comm.rank == 0:
+    print(f"L2 norm of u_: {u_norm}")
+    print(f"L2 norm of p_: {p_norm}")
---
+++ afsic/src/afsic/euler/dev2/readme.md
@@ -0,0 +1,20 @@
+## 方腔驱动流
+6. `a-5.py`
+    单核运行:
+    ```
+    triangle:
+    L2 norm of u_: 9.75001495005609
+    L2 norm of p_: 662470.6805256117
+    quadrilateral
+    L2 norm of u_: 9.680949899946235
+    L2 norm of p_: 380124.3585027092
+    ```
+    四核并行
+    ```
+    triangle
+    L2 norm of u_: 9.749200166149851
+    L2 norm of p_: 5889014.571645034
+    quadrilateral
+    L2 norm of u_: 9.681451558800033
+    L2 norm of p_: 388769.43117070774
+    ```
