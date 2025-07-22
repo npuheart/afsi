79d974b52fbcfc7e40a4ec8491a69f3a6d750e83
Pengfei Ma <mapengfei@mail.nwpu.edu.cn>
2025-07-22 23:05:55 +0800

    算例编号 2000

---
+++ afsic/demo/demo_2000/local_mesh.py
@@ -0,0 +1,56 @@
+from fenics import VectorFunctionSpace, FunctionSpace, Point
+from fenics import DirichletBC, Expression, Constant, interpolate
+from fenics import BoxMesh, Mesh, MeshEditor, CellType, XDMFFile
+from ibfenics1.cpp import IBMesh, IBInterpolation
+import numpy as np
+import os
+
+# Parameters
+nv = 0.01
+T = 10.0
+dt = 2e-4
+num_steps = int(T/dt)
+rho = 1.0
+Nl = 20
+Ne = 32
+dt_minimum = 1e-5
+
+# Mesh
+order_velocity = 2
+order_pressure = 1
+order_displacement = 2
+
+
+ib_mesh = IBMesh([Point(0, 0), Point(1, 1)], [Ne, Ne], order_velocity)
+fluid_mesh = ib_mesh.mesh()
+home_dir = os.path.expanduser("~")
+solid_mesh = Mesh()
+with XDMFFile(home_dir + f"/mesh-benchmark/B011-circle/mesh/circle_{Nl}.xdmf") as infile:
+    infile.read(solid_mesh)
+
+# Define function spaces
+Qf = FunctionSpace(fluid_mesh, "P", order_pressure)
+Vf = VectorFunctionSpace(fluid_mesh, "P", order_velocity)
+Vs = VectorFunctionSpace(solid_mesh, "P", order_displacement)
+
+# 初始化
+uf = interpolate(Expression(("x[0]", "x[1]"), degree=2), Vf)
+ib_mesh.build_map(uf._cpp_object)
+inter = IBInterpolation(ib_mesh, solid_mesh)
+us = interpolate(Expression(("x[0]", "x[1]"), degree=2), Vs)
+inter.evaluate_current_points(us._cpp_object)
+
+
+
+
+
+def calculate_fluid_boundary_conditions(Vf, Qf):
+    flow_velocity = Expression(("1.0", "0.0"), degree=1)
+    bcu_inflow = DirichletBC(Vf, flow_velocity, "near(x[1],1.0)")
+    bcu_wall = DirichletBC(Vf, Expression(("0.0", "0.0"), degree=1), 
+                           "near(x[0],0.0) || near(x[0],1) || near(x[1],0.0)")
+    bcp_outlet = DirichletBC(Qf, Expression("0.0", degree=1), "near(x[0],1.0) && near(x[1],1.0)", "pointwise")
+    bcu = [bcu_inflow, bcu_wall]
+    bcp = [bcp_outlet]
+    return bcu, bcp
+
---
+++ afsic/demo/demo_2000/ns.py
@@ -0,0 +1,116 @@
+from mpi4py import MPI
+from dolfinx.fem import (Function, functionspace,
+                         dirichletbc, locate_dofs_topological)
+from dolfinx.mesh import CellType, GhostMode, locate_entities, meshtags
+import numpy as np
+from petsc4py import PETSc
+from basix.ufl import element
+import dolfinx
+
+# 我自己写的包
+from afsic import IPCSSolver
+
+# Create mesh
+mesh = dolfinx.mesh.create_rectangle(
+    comm=MPI.COMM_WORLD,
+    points=((0.0, 0.0), (1.0, 1.0)),
+    n=(32, 32),
+    cell_type=CellType.triangle,
+    # cell_type=CellType.quadrilateral,
+    ghost_mode=GhostMode.shared_facet,
+)
+
+# Mark the boundaries
+mesh.topology.create_connectivity(1, 2) 
+marker_left, marker_right, marker_down, marker_up = 1, 2, 3, 4
+boundaries = [(1, lambda x: np.isclose(x[0], 0)),
+              (2, lambda x: np.isclose(x[0], 1)),
+              (3, lambda x: np.isclose(x[1], 0)),
+              (4, lambda x: np.isclose(x[1], 1))]
+
+
+facet_indices, facet_markers = [], []
+fdim = mesh.topology.dim - 1
+for (marker, locator) in boundaries:
+    facets = locate_entities(mesh, fdim, locator)
+    facet_indices.append(facets)
+    facet_markers.append(np.full_like(facets, marker))
+facet_indices = np.hstack(facet_indices).astype(np.int32)
+facet_markers = np.hstack(facet_markers).astype(np.int32)
+sorted_facets = np.argsort(facet_indices)
+facet_tag = meshtags(
+    mesh, fdim, facet_indices[sorted_facets], facet_markers[sorted_facets])
+
+
+# 下面时 `a-4.py` 的代码
+
+t = 0
+T = 10.5                       # Final time
+dt = 1 / 1600                 # Time step size
+num_steps = int(T / dt)
+rho = 1.0
+mu = 0.001
+
+v_cg2 = element("Lagrange", mesh.topology.cell_name(),
+                2, shape=(mesh.geometry.dim, ))
+v_cg1 = element("Lagrange", mesh.topology.cell_name(),
+                1, shape=(mesh.geometry.dim, ))
+s_cg1 = element("Lagrange", mesh.topology.cell_name(), 1)
+V_io = functionspace(mesh, v_cg1)
+V = functionspace(mesh, v_cg2)
+Q = functionspace(mesh, s_cg1)
+
+# Define boundary conditions
+fdim = mesh.topology.dim - 1
+gdim = mesh.geometry.dim
+tdim = mesh.topology.dim
+class UpVelocity():
+    def __init__(self, t):
+        self.t = t
+
+    def __call__(self, x):
+        values = np.zeros((gdim, x.shape[1]), dtype=PETSc.ScalarType)
+        values[0] = 1.0
+        values[1] = 0.0
+        return values
+
+
+# Inlet
+u_up = Function(V)
+up_velocity = UpVelocity(t)
+u_up.interpolate(up_velocity)
+bcu_up = dirichletbc(u_up, locate_dofs_topological(
+    V, fdim, facet_tag.find(marker_up)))
+# Walls
+u_nonslip = np.array((0,) * mesh.geometry.dim, dtype=PETSc.ScalarType)
+bcu_left = dirichletbc(u_nonslip, locate_dofs_topological(
+    V, fdim, facet_tag.find(marker_left)), V)
+bcu_right = dirichletbc(u_nonslip, locate_dofs_topological(
+    V, fdim, facet_tag.find(marker_right)), V)
+bcu_down = dirichletbc(u_nonslip, locate_dofs_topological(
+    V, fdim, facet_tag.find(marker_down)), V)
+bcu = [bcu_up, bcu_left, bcu_right, bcu_down]
+bcp = []
+
+# Define Solver
+ns_solver = IPCSSolver(V, Q, bcu, bcp, dt, rho, mu)
+xdmf_file = dolfinx.io.XDMFFile(mesh.comm, "x.xdmf", "w")
+xdmf_file.write_mesh(mesh)
+for i in range(num_steps):
+    # Update current time step
+    t += dt
+    # Update inlet velocity
+    up_velocity.t = t
+    u_up.interpolate(up_velocity)
+    ns_solver.solve_one_step()
+    u_io = Function(V_io)
+    u_io.interpolate(ns_solver.u_)
+    xdmf_file.write_function(u_io, t)
+    # xdmf_file.write_function(ns_solver.p_, t)
+    print(f"Step {i+1}/{num_steps}, Time: {t:.4f}")
+
+u_norm = dolfinx.la.norm(ns_solver.u_.x, dolfinx.la.Norm.l2)
+p_norm = dolfinx.la.norm(ns_solver.p_.x, dolfinx.la.Norm.l2)
+if mesh.comm.rank == 0:
+    print(f"L2 norm of u_: {u_norm}")
+    print(f"L2 norm of p_: {p_norm}")
